ce16a62d84b91c335cc09116c86a8d116a47759b
unknown <albertoherrera205@hotmail.com>
2024-05-22 15:52:22 -0500

    Paquete02-ArchivoEscritura/Lectura

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete0/ManejoArrayList4.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete0/ManejoArrayList4.java
@@ -20,13 +20,13 @@ public class ManejoArrayList4 {
         String linea = "Tara Hernandez;contratado";
 
         ArrayList<String> linea_partes = new ArrayList<>(
-                
                 Arrays.asList(linea.split(";")
-                
                 ) // ["Tara Hernandez", "contratado"]
         );
-        Profesor p = new Profesor(linea_partes.get(0), // Tara Hernandez
-                linea_partes.get(1)); // contratado;
+        String v1 = linea_partes.get(0);
+        String v2 = linea_partes.get(1);
+        Profesor p = new Profesor(v1, // Tara Hernandez
+                v2); // contratado;
 
         System.out.println(p);
     }
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete2/Ejecutor.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete2/Ejecutor.java
@@ -22,8 +22,13 @@ public class Ejecutor {
         ArchivoEscritura archivo = new ArchivoEscritura(nombreArchivo);
         
         // establecer el valor del atributo registro
+        // Almacena datos de un objeto en un archivo de texto en el cual
+        // lo concatena con un ';' y luego lo almacena en un archivo.txt
+        // tomando todos los datos que se requiera almacenar
         archivo.establecerRegistro(profesor1);
         // establecer en el archivo el atributo del registro
+        // Obtiene los valores de un archivo de texto.txt, y lo concatena en un
+        // list para poder trabajar con los datos obtenidos
         archivo.establecerSalida();
         
         // establecer el valor del atributo registro
